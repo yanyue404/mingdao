d4b2252f6caa027f63169c71916e5e345b5f5c05
RainBow <xiaoyueyue165@gmail.com>
2022-04-22 00:19:49 +0800

    feat: vuepress init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "王明道文库精选",
+  base: "/mingdao/",
+  themeConfig: {
+    nav: [
+      {
+        text: "王明道文库精选",
+        link: "/",
+      },
+      {
+        text: "重生真义",
+        link: "/qq",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/yanyue404/beyond-ui",
+      },
+    ],
+    sidebar: [
+      ["/", "简介"],
+      ["/wmd1", "王明道文库精选（一）"],
+    ],
+    displayAllHeaders: true,
+  },
+};
